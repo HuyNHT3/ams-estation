7b58eb876680aab2d985a231f7ada42b784609c5
HuyErik <thanhhuyst05@gmail.com>
2023-12-08 20:16:04 +0700

    apply scss

---
+++ src/main/webapp/src/component/CheckVocabulary.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Banner from './shared-component/Banner'
+
+function CheckVocabulary() {
+  return (
+   <>
+      <Banner/>
+   </>
+  );
+}
+
+export default CheckVocabulary;
