1ca5d5209313db3fb7f138a32d5411ccd4852996
HuyErik <thanhhuyst05@gmail.com>
2023-12-18 20:40:41 +0700

    implement check vocabulary cpn

--- src/main/webapp/src/component/vocabulary-check/CheckVocabularyBody.js
+++ src/main/webapp/src/component/vocabulary-check/CheckVocabularyBody.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { getAllVocabulary } from '../../api/VocabularyDataSource';
 import './CheckVocabulary.scss'
 import CheckVocabularyItem from './CheckVocabularyItem';
@@ -9,6 +9,10 @@ function CheckVocabularyBody() {
     const [vocabList, setVocabList] = useState(null);
     let [currentWord, setCurrentWord] = useState(null);
     let [isFinish, setIsFinish] = useState(false);
+    let [rightAnswer, setRightAnswer] = useState([]);
+    let [wrongAnswer, setWrongAnswer] = useState([]);
+    let [userAnswer, setUserAnswer] = useState('');
+    let [count, setCount] = useState(0);
 
     useMemo(() => {
         getAllVocabulary().then((res) => {
@@ -17,18 +21,41 @@ function CheckVocabularyBody() {
         })
     }, [vocabList === '']);
 
-    function handleOnKeyPress(e) {
-        e.preventDefault();
 
+
+    function handleOnKeyPress(e) {
+        
         if (e.key === 'Enter') {
+    
+             console.log("userAnswer: " + userAnswer);
+
             if (vocabIndex < vocabList.vocabularyList.length) {
+                if(userAnswer === currentWord.name) {
+                    var tempAnswer = rightAnswer;
+                            tempAnswer.push(currentWord);
+                            setRightAnswer(tempAnswer);
+                            setCount(count++);
+                            console.log("rightAnswer: " + JSON.stringify(rightAnswer));
+                            console.log("count: " + count);
+                 }
+                 else {
+                    var tempAnswer = wrongAnswer;
+                            tempAnswer.push(currentWord);
+                            setWrongAnswer(tempAnswer);
+                            console.log("wrongAnswer: " + JSON.stringify(wrongAnswer))
+                 }
+
                 if (vocabList) {
                     setCurrentWord(vocabList.vocabularyList[vocabIndex + 1]);
                 }
                 setVocabIndex(vocabIndex++);
             }
 
-            if (vocabIndex >= vocabList.vocabularyList.length) setIsFinish(true);
+            if (vocabIndex === vocabList.vocabularyList.length) {
+                setIsFinish(true);
+                setRightAnswer([]);
+                setWrongAnswer([]);
+            }
         }
     }
 
@@ -36,6 +63,10 @@ function CheckVocabularyBody() {
         <div className='vocab-body'>
             <div className='row content'>
                 <div className='col-12 shadow p-3 mb-3 bg-body rounded'>
+                    <div className='text-end'>
+                        <span className='text-success fw-bolder'>Right: {count}</span> |
+                        {!!wrongAnswer && (<span className='text-danger fw-bolder'> Wrong: {wrongAnswer.length}</span>)}
+                    </div>
                     {!isFinish && (
                         <CheckVocabularyItem
                             vocabList={vocabList}
@@ -45,23 +76,23 @@ function CheckVocabularyBody() {
                     {!!isFinish && (
                         <h1>Congratulation, you finshed !!!</h1>
                     )}
-                    
+
                 </div>
             </div>
             <div className='row inputBox'>
                 <div className='col-12 p-0'>
-                    <form>
-                        <div class="">
-                            <input
-                                type="text"
-                                class="form-control shadow bg-body rounded"
-                                id="exampleInputEmail1"
-                                aria-describedby="emailHelp"
-                                placeholder='When start checking progress for learning vocabulary, please typing word here, and press enter after finish typing!'
-                                onKeyDown={handleOnKeyPress}
-                            />
-                        </div>
-                    </form>
+                    <div class="">
+                        <input
+                            autoFocus
+                            type="text"
+                            class="form-control shadow bg-body rounded"
+                            id="exampleInputEmail1"
+                            aria-describedby="emailHelp"
+                            placeholder='When start checking progress for learning vocabulary, please typing word here, and press enter after finish typing!'
+                            onKeyDown={handleOnKeyPress}
+                            onChange={(e) => setUserAnswer(e.target.value)}
+                        />
+                    </div>
                 </div>
             </div>
         </div>
